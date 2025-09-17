9bc536d4d498d652c647c88a32b447dd70b833d2
kleinfrankenheim <126899813+kleinfrankenheim@users.noreply.github.com>
2025-09-17 20:19:34 +0500

    Feature added. Needs testing.

--- replace_train.py
+++ replace_train.py
@@ -1,6 +1,7 @@
 import enum
 import json
 import re
+from msvcrt import getche
 from typing import List, Dict, Callable
 
 
@@ -102,8 +103,18 @@ INPUT_FILTER = {
 }
 
 MOVES_FILTER = {
-    r'SLS': 'qcf'
+    'bryan': [r'sls', 'qcf'],
+    'steve': [r'spin', '3+4']
 }
+
+
+
+# Function to open files
+def open_frame_data(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+
+        return data
 
 
 # Function to filter inputs
@@ -111,20 +122,59 @@ def filter_input(input_foo):
     for key_foo, value_foo in INPUT_FILTER.items():
         input_foo = re.sub(key_foo, value_foo, input_foo, flags=re.I)
 
-    for key_foo1, value_foo1 in MOVES_FILTER.items():
-        input_foo = re.sub(key_foo1, value_foo1, input_foo, flags=re.I)
-
     return input_foo.lower()
 
 
-def compare_inputs(data, command_input):
+# Function to filter to treat movement inputs as stance moves names/
+# For example:
+# BRYAN - SLS should be treated as qcf.
+# If user inputs (!fd bryan qcf1+2) it should give SLS.1+2 instead. Because they are basically the same.
+# MOVES_FILTER is a dictionary that built like this: { characters: [(SLS), (qcf1+2)] }
+# The odd number of the list(SLS) is the input that needs to be changed and the even number is the input that should be
+# treated(qcf). SLS>qcf.
+
+def filter_moves(input_foo, character_foo):
+    counter = 1
+    odd_list = []
+    even_list = []
+
+    # Iterating through main dictionary.
+    for ch_move_pair in MOVES_FILTER.items():
+        counter = 1
+        character = ch_move_pair[0]
+        moves = ch_move_pair[1]
+        if character == character_foo:
+
+            # Creating 2 lists:
+            # First one is for odd (check 125-126 lines).
+            # Second one is for even.
+            for move in moves:
+                if counter % 2 == 1:
+                    odd_list.append(move)
+                elif counter % 2 == 0:
+                    even_list.append(move)
+                counter = counter + 1
+
+            # Now we should create a dictionary which should just look like INPUT_FILTER
+            # {(string that needs to be changed): (the string that needs to be changed into)}
+            filter_dict = dict(zip(odd_list, even_list))
+
+            for key_foo, value_foo in filter_dict.items():
+                input_foo = re.sub(key_foo, value_foo, input_foo, flags=re.I)
+
+    return input_foo
+
+
+def compare_inputs(data_given, character_name, command_input):
     found_move = ''
     found_input = filter_input(command_input)
+    found_input = filter_moves(found_input, character_foo=character_name)
 
     counter = 0
     # Iterating through every dict while changing the command key values and comparing them to input
-    for any_move in data:
+    for any_move in data_given:
         found_move = filter_input(any_move['command'])
+        found_move = filter_moves(input_foo=found_move, character_foo=character_name)
         # print(any_move['command'])
 
         # only comparing when the whole input filter is finished
@@ -137,11 +187,11 @@ def compare_inputs(data, command_input):
     return None
 
 
-def get_character_by_name(user_input):
+def get_character_by_name(character_input):
     result = None
 
     for any_character in CharacterName:
-        if user_input.lower() == any_character.value:
+        if character_input.lower() == any_character.value:
             result = any_character.value
             return result
 
@@ -150,22 +200,20 @@ def get_character_by_name(user_input):
     # matched any_key value is returned, if user_input matches any of the aliases from any list
     if result is None:
         for any_key, any_value_list in Character_alias.items():
-            if user_input.lower() in any_value_list:
+            if character_input.lower() in any_value_list:
                 result = any_key.value
                 return result
 
     return None
 
 
-'''
 # Used for debugging purposes
 user_input = "kaz"
-character = get_character_by_name(user_input)
+filtered_input = filter_input('qcf1+2')
+character_name_gotten = get_character_by_name('byron')
+print(filter_moves('sls1+2', 'bryan'))
 
-if character:
-    print(f"Matched character: {character}")
-else:
-    print(f"'{user_input}' is not a valid character.")
+data = open_frame_data('./output/bryan.json')
+printable_junk = compare_inputs(data, character_name_gotten, 'qcf1+2')
 
-print(compare_inputs())
-'''
+print(printable_junk)
